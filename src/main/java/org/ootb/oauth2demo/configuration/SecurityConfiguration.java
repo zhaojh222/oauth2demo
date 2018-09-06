03740210819448b1b9ccfadd93543e2d7e76b51d
vn0nzx5 <zhaojh222@gmaio.com>
2018-09-05 21:52:08 -0700

    init

---
+++ src/main/java/org/ootb/oauth2demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package org.ootb.oauth2demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
---
+++ src/main/java/org/ootb/oauth2demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.ootb.oauth2demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/hi/{name}")
+    public String sayHi(@PathVariable String name){
+        return "Hi:" + name;
+    }
+}
